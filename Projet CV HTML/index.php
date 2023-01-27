5c594839a5212c10a0213bb4db8903416d018b93
Bluebender <olliviev@gmail.com>
2023-01-27 09:59:27 +0000

    Sauvegarde de 11h

---
+++ Projet CV HTML/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$formData = [
+    'firstName' => 'nom',
+    'lastName' => 'lastname',
+    'email' => 'email',
+    'phone' => 'telephone',
+    'linkedin' => 'linkedin',
+    'github' => 'github',
+    'biographie' => 'biographie',
+    'loisir_1' => 'loisir_1',
+    'loisir_2' => 'loisir_2',
+    'loisir_3' => 'loisir_3',
+    'loisir_4' => 'loisir_4',
+    ];
+
+
+?>
